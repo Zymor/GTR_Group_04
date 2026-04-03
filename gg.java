fae5a9d392c960c47f93b61afefab7ac5ca8f191
Zymor <163753820+Zymor@users.noreply.github.com>
2026-04-03 13:37:19 +0700

    delete gg

--- gg.java
+++
@@ -1,9 +0,0 @@
-public class gg {
-    // This is a simple Java program that prints "Hello World" to the console.
-    //ffffff       fghj GVHHJH
-
-    //dfghjl
-    //FDXDS;;;;;
-
-    //hello world
-}
