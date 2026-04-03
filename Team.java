4bc39d5466fdae662bad1f22efc959e9e5faca2f
Mr.phannang <phannangtak@gmail.com>
2026-04-03 14:13:14 +0700

    update

--- Team.java
+++ Team.java
@@ -1,6 +1,11 @@
 public class Team {
     // atribute ms.davy
     // constuctor mr.phanang
+    public static void main(String[] args) {
+        
+    }
+        
+    
     // method sum ms.phaleng
     // method max mr.yun
 }
