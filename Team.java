e33985c1178247f9667f00822ccca2bf712cf3c2
phalengsophal-arch <phalengsophal@gmail.com>
2026-04-03 14:13:57 +0700

    ready

--- Team.java
+++ Team.java
@@ -2,5 +2,14 @@ public class Team {
     // atribute ms.davy
     // constuctor mr.phanang
     // method sum ms.phaleng
+    public int sum(int[] input){
+        int sum = 0;
+        for(int i = 0; i < input.length; i++){
+            sum += input[i];
+        }
+        System.out.println("The sum is: " + sum);
+        return sum;
+
+    }
     // method max mr.yun
 }
