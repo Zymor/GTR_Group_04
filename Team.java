50c97f217575d0db26724a08ec0d66a42a16ef93
yunn27-spec <chhingyunn432@gmail.com>
2026-04-03 14:19:20 +0700

    update

--- Team.java
+++ Team.java
@@ -3,4 +3,15 @@ public class Team {
     // constuctor mr.phanang
     // method sum ms.phaleng
     // method max mr.yun
+public int max(int[] input){
+    int max = input[0];
+    for(int i = 1; i < input.length; i++){
+        if(input[i]> max){
+            max = input[i];
+        }
+    }
 }
+ system.out.println("Max:"+ max);
+ retuen max;
+}
+      
